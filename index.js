652bd533dc38becc92fd27ea681d39be899532c8
creative-buffer-9578 <cmuusha@gmail.com>
2020-06-28 23:58:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ function add(a,b) {
   return a+b
 }
 
-subtract add(a,b) {
+function subtract add(a,b) {
   return a-b
 }
 
-multiple add(a,b) {
+function multiple add(a,b) {
   return a*b
 }
 
-divide add(a,b) {
+function divide add(a,b) {
   return a/b
 }
